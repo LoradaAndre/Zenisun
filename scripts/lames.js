0d3eb843bdfa8acb2a3e6981304eecebd155bcee
Lorada ANDRE <dada.andre2@gmail.com>
2021-03-22 16:38:14 +0100

    feat: Déplacement du moteur0 selon la barre et les boutons

--- scripts/lames.js
+++ scripts/lames.js
@@ -1,17 +1,40 @@
+let config = 1;
+
+let valueMotor0;
 let valueMotor1;
-let valueMotor2;
+
+let valueMaxMotor0;
+let valueMaxMotor1;
+
+
+$(document).ready(function (){
+    
+    $(zone).change(function(){
+        let val = parseInt(zone.value*valueMaxMotor0/100);
+        console.log(val)
+        deplacementLames(1,val)
+    });
+    setInterval(function(){ 
+        lectureCarte();
+        if(valueMotor0 != undefined){
+            refreshBarre();
+        }
+    }, 100);
+
+});
 
 function lectureCarte(){
     my_current_automatum_cmd = "&ID=0";
     $.ajax({
-        url: "cgi/zns.cgi?cmd=d&p=ios"+my_current_automatum_cmd,
+        url: "../cgi/zns.cgi?cmd=d&p=ios"+my_current_automatum_cmd,
         context: document.body
       }).done(function(data) {
-          valueMotor1 = getMotorValue(data, 24);
-          valueMotor2 = getMotorValue(data, 25)
-          console.log(parseInt(pourcentage))
+          valueMotor0 = parseInt(getMotorValue(data, 24));
+          valueMaxMotor0 = parseInt(getMotorMaxValue(data, 24));
+          valueMotor1 = parseInt(getMotorValue(data, 25));
+          valueMaxMotor0 = parseInt(getMotorMaxValue(data, 25));
       }).fail(function() {
-          alert("error")
+          alert("Lecture de la carte échouée")
       });	
 }
 
@@ -21,5 +44,74 @@ function getMotorValue(data, input){
     return newVal[0]*100/newVal[1]
 }
 
-lectureCarte(); 
+function getMotorMaxValue(data, input){
+    let valMotor = data.all[input].textContent
+    let newVal = valMotor.split(";")
+    return newVal[1];
+}
 
+function refreshBarre(){
+
+    let valueAllMotors = [valueMotor0, valueMotor1];
+
+    let barre = document.querySelectorAll(".zone-range-wrap");
+    
+    for(let i = 0; i< barre.length; i++){
+        let bubble = barre[i].querySelector(".bubble");
+        let contenuVal = barre[i].querySelector(".value-range-wrap");
+        console.log(valueAllMotors[i])
+        if(config == 1){
+            setOffsetBubble(bubble, contenuVal, valueMotor0);
+        }else{
+            setOffsetBubble(bubble, contenuVal, valueAllMotors[i]);
+        }
+        
+    }
+    console.log()
+}
+
+$("h3").click(function(){
+    if(this.id == "1-LO1"){
+        deplacementLames(1, 0)
+    }
+    if(this.id == "1-LO2"){
+        deplacementLames(1, parseInt(valueMaxMotor0/4));
+    }
+    if(this.id == "1-LO3"){
+        deplacementLames(1, parseInt(valueMaxMotor0/2))
+    }
+    if(this.id == "1-LO4"){
+        deplacementLames(1, parseInt(valueMaxMotor0/4*3))
+    }
+    if(this.id == "1-LO5"){
+        deplacementLames(1, valueMaxMotor0)
+    }
+    if(this.id == "2-LO1"){
+        deplacementLames(1, 0)
+    }
+    if(this.id == "2-LO2"){
+        deplacementLames(1, parseInt(valueMaxMotor1/4))
+    }
+    if(this.id == "2-LO3"){
+        deplacementLames(1, parseInt(valueMaxMotor1/2))
+    }
+    if(this.id == "2-LO4"){
+        deplacementLames(1, parseInt(valueMaxMotor1/4*3))
+    }
+    if(this.id == "2-LO5"){
+        deplacementLames(1, valueMaxMotor1)
+    }
+});
+
+let zone = document.querySelector(".wrap-lames-1 .range");
+
+function deplacementLames(moteur, valeur){ 
+    $.ajax({
+        url: '../cgi/zns.cgi?cmd=m&m=' + moteur + '&p=' + valeur,
+        context: document.body
+      }).done(function(data) {
+         alert('done')
+      }).fail(function() {
+          alert("Déplacement de la lame échoué")
+      });
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,136 +1,129 @@
-    /* ========================== FARBTASTIC  ========================== */
+/* ========================== FARBTASTIC  ========================== */
    
 
 
  /* ========================== FOR RANGE SLIDER ========================== */
 
-const allRanges = document.querySelectorAll(".zone-range-wrap");
-allRanges.forEach((wrap) => {
-  const range = wrap.querySelector(".range");
-  const bubble = wrap.querySelector(".bubble");
-  const contenuVal = wrap.querySelector(".value-range-wrap");
-
-  range.addEventListener("input", () => {
-    setBubble(range, bubble, contenuVal);
-  });
-
-  // setting bubble on DOM load
-  // setBubble(range, bubble, contenuVal);
-});
+ const allRanges = document.querySelectorAll(".zone-range-wrap");
+ allRanges.forEach((wrap) => {
+   const range = wrap.querySelector(".range");
+   const bubble = wrap.querySelector(".bubble");
+   const contenuVal = wrap.querySelector(".value-range-wrap");
  
-function setBubble(range, bubble, contenuVal) {
-
-  const val = range.value;
-
-  const min = range.min || 0;
-  const max =  range.max || 100;
-
-  const offset = Number(((val - min) * 100) / (max - min));
-
-  bubble.textContent = val;
-  console.log("coucou")
+   range.addEventListener("input", () => {
+     setBubble(range, bubble, contenuVal);
+   });
+ 
+   // setting bubble on DOM load
+   // setBubble(range, bubble, contenuVal);
+ });
   
-  
-  
-  if((contenuVal != null) && (contenuVal.textContent != null)){
-    contenuVal.textContent = val + " W";
-  }
-  
-
-  // yes, 14px is a magic number
-  bubble.style.left = `calc(${offset}% - 14px)`;
-}
-
-function setOffsetBubble(bubble, contenuVal, number) {
-  console.log("coucou")
-  const offset = number;
-
-  bubble.textContent = number;
-  
-  if((contenuVal != null) && (contenuVal.textContent != null)){
-    contenuVal.textContent = number + " W";
-  }
-
-
-  // yes, 14px is a magic number
-  bubble.style.left = `calc(${offset}% - 14px)`;
-}
-
-
-/* ========================== Boutons multiples actualisation ========================== */
-
-
-function permutationBoutonGradateurLed(){
-  permutationBouton(".list_of_buttons_grad_LED h3");
-}
-
-function permutationBoutonSuiviSolaire(){
-  permutationBouton(".list_of_buttons_suivi_sol h3");
-}
-
-function permutationBoutonA(){
-  permutationBouton(".boutons_colorisation h3");
-}
-
-function permutationBoutonLames1(){
-  permutationBouton(".lbl1 h3");
-}
-
-function permutationBoutonLames2(){
-  permutationBouton(".lbl2 h3");
-}
-
-function permutationBouton(boutons){
-  // Récupération de chaque boutons et stockage des id
-let tabAllButtons = [];
-
-  $(boutons).each(function(){
-    tabAllButtons.push($(this).attr("id"));
-  });
-
-  // En cas de clic sur un bouton
-  $(boutons).click(function(){
-    // Sur le bouton cliqué
-    $(this).css("background-color","#52808B");
-    $(this).css("color","white");
-    let actuel = $(this).attr("id");
-
-    let a = this.parentNode.parentNode.children[1];
-    const bubble = a.querySelector(".bubble");
-    const contenuVal = a.querySelector(".value-range-wrap");
-  
-    let n = $(this).html()
-    let newN = n.substring(0, n.length - 1);
-    setOffsetBubble(bubble, contenuVal, newN)
-
-    // Sur tout les autres boutons
-    for(let i = 0; i < tabAllButtons.length; i++){
-      if(tabAllButtons[i] != actuel){
-        $("#" + tabAllButtons[i]).css("background-color","white");
-        $("#" + tabAllButtons[i]).css("color","black");
-      }
-    }
-  });
-}
-
-permutationBoutonSuiviSolaire();
-permutationBoutonGradateurLed();
-permutationBoutonA();
-permutationBoutonLames1();
-permutationBoutonLames2();
-
-/* ========================== Boutons changement coloriastion ========================== */
-
-
-$("#R1").click(function(){
-  $(".roue_chroma").hide();
-  $(".reglages_rvb").show();
-});
-
-$("#R2").click(function(){
-  $(".roue_chroma").show();
-  $(".reglages_rvb").hide();
-});
-
-$(".roue_chroma").hide();
-$(".reglages_rvb").show();
+ function setBubble(range, bubble, contenuVal) {
+ 
+   const val = range.value;
+ 
+   const min = range.min || 0;
+   const max =  range.max || 100;
+ 
+   const offset = Number(((val - min) * 100) / (max - min));
+ 
+   bubble.textContent = val;
+ 
+   // yes, 14px is a magic number
+   bubble.style.left = `calc(${offset}% - 14px)`;
+ }
+ 
+ function setOffsetBubble(bubble, contenuVal, number) {
+    const offset = number;
+    bubble.textContent = number;
+    
+    if((contenuVal != null) && (contenuVal.textContent != null)){
+        if(contenuVal.parentNode.classList.contains("wrap-lames")){
+            contenuVal.textContent = number + "%";
+        }
+   }
+ 
+ 
+   // yes, 14px is a magic number
+   bubble.style.left = `calc(${offset}% - 14px)`;
+ }
+ 
+ 
+ /* ========================== Boutons multiples actualisation ========================== */
+ 
+ 
+ function permutationBoutonGradateurLed(){
+   permutationBouton(".list_of_buttons_grad_LED h3");
+ }
+ 
+ function permutationBoutonSuiviSolaire(){
+   permutationBouton(".list_of_buttons_suivi_sol h3");
+ }
+ 
+ function permutationBoutonA(){
+   permutationBouton(".boutons_colorisation h3");
+ }
+ 
+ function permutationBoutonLames1(){
+   permutationBouton(".lbl1 h3");
+ }
+ 
+ function permutationBoutonLames2(){
+   permutationBouton(".lbl2 h3");
+ }
+ 
+ function permutationBouton(boutons){
+   // Récupération de chaque boutons et stockage des id
+ let tabAllButtons = [];
+ 
+   $(boutons).each(function(){
+     tabAllButtons.push($(this).attr("id"));
+   });
+ 
+   // En cas de clic sur un bouton
+   $(boutons).click(function(){
+     // Sur le bouton cliqué
+     $(this).css("background-color","#52808B");
+     $(this).css("color","white");
+     let actuel = $(this).attr("id");
+ 
+     let a = this.parentNode.parentNode.children[1];
+     const bubble = a.querySelector(".bubble");
+     const contenuVal = a.querySelector(".value-range-wrap");
+   
+     let n = $(this).html()
+     let newN = n.substring(0, n.length - 1);
+     setOffsetBubble(bubble, contenuVal, newN)
+ 
+     // Sur tout les autres boutons
+     for(let i = 0; i < tabAllButtons.length; i++){
+       if(tabAllButtons[i] != actuel){
+         $("#" + tabAllButtons[i]).css("background-color","white");
+         $("#" + tabAllButtons[i]).css("color","black");
+       }
+     }
+   });
+ }
+ 
+ permutationBoutonSuiviSolaire();
+ permutationBoutonGradateurLed();
+ permutationBoutonA();
+ permutationBoutonLames1();
+ permutationBoutonLames2();
+ 
+ /* ========================== Boutons changement coloriastion ========================== */
+ 
+ 
+ $("#R1").click(function(){
+   $(".roue_chroma").hide();
+   $(".reglages_rvb").show();
+ });
+ 
+ $("#R2").click(function(){
+   $(".roue_chroma").show();
+   $(".reglages_rvb").hide();
+ });
+ 
+ $(".roue_chroma").hide();
+ $(".reglages_rvb").show();
+ 
