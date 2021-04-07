54a4138358caf810633b502f103fea1a4985eea2
Lorada ANDRE <dada.andre2@gmail.com>
2021-04-07 11:45:26 +0200

    feat: Ajout et fonctionnement de tout les widgets

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -1,12 +1,31 @@
-let valEclairage;
-let valueMotor;
+let valEclairage1;
+let valEclairage2;
+
+let valueMotor1;
+let valueMotor2;
 
 $(document).ready(function (){
     setInterval(function(){ 
         lectureCarte();
-        if(valueMotor != undefined){
-            setValueWidgetLames(valueMotor)
-            refreshCanvas()
+        if(valueMotor1 != undefined){
+            let canvasLames1 = document.querySelector(".L1-canvas");  
+            setValueWidgetLames(valueMotor1, canvasLames1)
+            refreshCanvas(valueMotor1, canvasLames1)
+        }
+        if(valueMotor2 != undefined){
+            let canvasLames2 = document.querySelector(".L2-canvas");  
+            setValueWidgetLames(valueMotor2, canvasLames2)
+            refreshCanvas(valueMotor2, canvasLames2)
+        }
+        if(valEclairage1 != undefined){
+            let canvasEclairage1 = document.querySelector(".E1-canvas"); 
+            setValueWidgetLames(valEclairage1, canvasEclairage1)
+            refreshCanvas(valEclairage1, canvasEclairage1)
+        }
+        if(valEclairage2 != undefined){
+            let canvasEclairage2 = document.querySelector(".E2-canvas"); 
+            setValueWidgetLames(valEclairage2, canvasEclairage2)
+            refreshCanvas(valEclairage2, canvasEclairage2)
         }
     }, 1000);
 });
@@ -17,8 +36,15 @@ function lectureCarte(){
         url: "cgi/zns.cgi?cmd=d&p=ios"+my_current_automatum_cmd,
         context: document.body
       }).done(function(data) {
-          valueMotor = parseInt(getMotorValue(data, 24));
-          console.log(valueMotor)
+          valueMotor1 = parseInt(getMotorValue(data, 24));
+          valueMotor2 = parseInt(getMotorValue(data, 25));
+
+          //BB1
+          valEclairage1 = parseInt(getIntensite(data, 14)); //14: <GPO 6>
+          //BB2
+          valEclairage2 = parseInt(getIntensite(data, 15)); //15: <GPO 7>
+
+          console.log(valueMotor1)
       }).fail(function() {
         //   alert("Lecture de la carte échouée")
       });	
@@ -31,13 +57,21 @@ function getMotorValue(data, input){
     return newVal[0]*100/newVal[1]
 }
 
-function refreshCanvas(){
-    let canvasLames = document.querySelector(".lames-orientable_widget canvas");  
-    circle(valueMotor, canvasLames);
+//Récupère et converti l'intensité (0 à 255) en pourcentage
+function getIntensite(data, input){
+    let ledIntensite = data.all[input].textContent
+    let newVal = ledIntensite.split(";")
+    return newVal[0]*100/255;
+}
+
+function refreshCanvas(val, canvasType){
+    console.log("dans le refresh =>")
+    console.log(canvasType)
+    circle(val, canvasType);
     
 }
 
-function setValueWidgetLames(value){
-    let canvasLames = document.querySelector(".lames-orientable_widget canvas");
-    canvasLames.setAttribute("value", value);
+function setValueWidgetLames(value, canvasType){
+    console.log(canvasType)
+    // canvasType.setAttribute("value", value);
 }
