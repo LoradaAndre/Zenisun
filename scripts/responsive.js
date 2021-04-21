8d8522659ef7bec165695a365a0f6eb24f20792d
Lorada ANDRE <dada.andre2@gmail.com>
2021-04-21 09:20:33 +0200

    feat: Restructuration de la page html éclairage

---
+++ scripts/responsive.js
@@ -0,0 +1,60 @@
+
+// $(".overlay").click(function(){
+//     $(this).css("display","none");
+//     $(".navigation_bar").css({
+//         "display" : "block",
+//         "width" : "70%",
+//         "z-index" : "3"
+//     });
+
+//     $(".icon_navbar img").css({
+//         "width" : "30px",
+//         "vertical-align" : "middle",
+//         "margin" : "10px"
+//     });
+//     $(".categorie a").css({
+//         "display" : "flex"
+//     })
+//     $(".categorie_spe").css({
+//         "display" : "flex",
+//         "justify-content" : "space-between"
+//     })
+//     $(".categorie_spe img").css({
+//         "margin-top" : "auto",
+//         "margin-bottom" : "auto"
+//     })
+//     $(".nom_widget").css({
+//         "margin-top" : "auto",
+//         "margin-bottom" : "auto"
+//     })
+// });
+
+// $(".close").click(function(){
+//     $(this).css("float","left");
+
+//     $(".navigation_bar").css({
+//         "display" : "none"
+//     });
+
+//     $(".overlay").css("display","block");
+// });
+
+function resizeWindowEvent(){
+    let widthNavBar = $(".navigation_bar").outerWidth()
+    let largeurEcran = window.innerWidth;
+    let diff = largeurEcran - widthNavBar - 20
+    $("main").css({
+        "width": diff + "px",
+        "margin-right" : "0"
+    })
+
+    // $(".contenu-main").css({
+    //     "width": "100%",
+    //     // "margin" : "auto"
+    // })
+}
+
+resizeWindowEvent()
+window.onresize = resizeWindowEvent;
+// // console.log(document.querySelector("."))
+// // offsetHeight
